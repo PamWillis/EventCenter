6cbecadcda4604310f9ee5e0fd5f0c9c569f8c98
David H <darvidian123456@gmail.com>
2024-01-07 21:04:08 -0500

    home page route/style

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -9,22 +9,29 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to my website!</h1>
-      <div className="image-links-container">
+      <div className="">
         <Link to="/AboutUs">
-        <img src={aboutImage} alt="about me" />
+        <img src={aboutImage}
+        style={{ width: "750px", height: "500px" }}alt="about me" />
           <p>About Us</p>
         </Link>
         <Link to="/product">
-          <img src={buyImage} alt="Buy-Now" />
+          <img src={buyImage} 
+          style={{ width: "750px", height: "500px" }}
+          alt="Buy-Now" />
           <p>Products</p>
         </Link>
         <Link to="/project">
-          <img src={projectImage} alt="Project" />
+          <img src={projectImage} 
+          style={{ width: "750px", height: "500px" }}
+          alt="Project" />
           <p>Projects</p>
         </Link>
         <Link to="/contact">
-          <img src={resumeImage} alt="Resume" />
-          <p>Contact\</p>
+          <img src={resumeImage} 
+          style={{ width: "750px", height: "500px" }}
+          alt="Resume" />
+          <p>Contact</p>
         </Link>
       </div>
     </div>
