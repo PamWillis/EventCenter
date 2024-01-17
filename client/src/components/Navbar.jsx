145d03dbb0a63d6f208f15e7a086e099483f6439
jman1359 <josiahsmith1359@gmail.com>
2024-01-17 17:19:37 -0500

    final styling tweeks

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,32 +6,35 @@ import { Button } from "@material-tailwind/react";
 import '../App.css';
 
 const Navbar = () => {
-  // const [showModal, setShowModal] = useState(false);
+
+// Hides/Shows mobile menu
+
   const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
-  // const [activeTab, setActiveTab] = useState('home');
+
+  //Signup
 
   const handleLoginSignupClick = () => {
     setShowModal(true);
   };
 
+  //Logout
+
   const handleLogout = () => {
     AuthService.logout();
   };
+
+  //Mobile menu toggle button
+
   const handleMobileMenuToggle = () => {
     setMobileMenuVisible(!mobileMenuVisible);
-    const setHeight = () => document.getElementById('navbar').style.height = "50%";
-    setHeight()
+    
   };
 
-  // const handleTabClick = (tab) => {
-  //   setActiveTab(tab);
-  // }
-
   return (
-    <nav className={`bg-gradient-to-r from-cyan-900 to-green-800 py-1.75 sticky top-0 z-50 ${mobileMenuVisible ? 'header-shrink' : ''}`} id='navbar'>
+    <nav className={`bg-gradient-to-r from-cyan-900 to-green-800 py-1.75 lg:sticky top-0 z-50 ${mobileMenuVisible ? 'header-shrink' : ''}`} id='navbar'>
       <div className="container mx-auto flex justify-between items-center">
 
-        {/* Logo */}
+        {/* EventsCenter Logo */}
 
         <div className="flex items-center align-center">
           <a href="/"><img src={logoImage} alt="EventCenter" className="h-14 mr-3 mb-3" style={{ height: "60px" }} /></a>
@@ -96,7 +99,7 @@ const Navbar = () => {
         {/* MOBILE NAVBAR */}
 
       {/* Mobile Menu Toggle Button */}
-        <div className="md:hidden sm:h-1/2">
+        <div className="md:hidden">
           <Button onClick={handleMobileMenuToggle} id="mobile-menu-toggle" className="text-white focus:outline-none">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
@@ -104,7 +107,10 @@ const Navbar = () => {
           </Button>
         </div>
       </div>
-      <div className={`md:hidden ${mobileMenuVisible ? 'mobile-menu-show' : 'mobile-menu-hide'} sticky`} id="mobile-menu">
+
+      {/* Mobile Navbar Links*/}
+
+      <div className={`md:hidden ${mobileMenuVisible ? 'mobile-menu-show' : 'mobile-menu-hide'} md:flex justify-center`} id="mobile-menu">
 
         <NavLink to="/"
           className="nav-link block py-2 px-4 text-sm text-white">
@@ -140,7 +146,6 @@ const Navbar = () => {
             Login/Signup
           </NavLink>
         )}
-
       </div>
     </nav>
   );
